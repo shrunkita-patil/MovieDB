53a3b164504cf5967a6d8d691d17786edcb6f898
shivdas-OSP <shivdas@osplabs.com>
2020-09-05 11:04:03 +0530

    initial commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Riken Shah on 05/09/20.
+//  Copyright © 2020 Shrunkita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
